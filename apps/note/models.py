c6c48bb81fea2ea74045b9a57d3600fd270db4b0
innaisareva <inna8110302@gmail.com>
2024-12-17 10:00:51 +0200

    minor updates

--- apps/note/models.py
+++ apps/note/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     content = models.CharField(max_length=510, verbose_name="Контент")
-    created_at = models.DateTimeField(auto_now_add=True,  verbose_name="Дата створення")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата оновлення")
+    created_at = models.DateTimeField(auto_now_add=True,  verbose_name="Дата та час створення")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата та час оновлення")
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Користувач")
 
     def __str__(self) -> str:
